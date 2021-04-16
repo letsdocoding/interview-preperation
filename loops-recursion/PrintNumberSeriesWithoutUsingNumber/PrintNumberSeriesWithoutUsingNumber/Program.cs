c25051b542b1d7c7d03b6fe35be324f3b14a6d0d
Vineet Yadav <vineetyadav@msn.com>
2021-04-16 14:50:46 +0530

    print series without using number

---
+++ loops-recursion/PrintNumberSeriesWithoutUsingNumber/PrintNumberSeriesWithoutUsingNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace PrintNumberSeriesWithoutUsingNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //PrintFirstSetOfNumbersWitLoop();
+            PrintFirstSetOfNumbers();
+            Console.WriteLine("The End");
+        }
+
+        private static void PrintFirstSetOfNumbers()
+        {
+            int number = 'Z' - 'A';
+            for (int i = 'A'-'A'; i <= number; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        private static void PrintFirstSetOfNumbersWitLoop()
+        {
+            int number = 25;
+            for (int i = 0; i <= number; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
